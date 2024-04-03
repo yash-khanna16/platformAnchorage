dd3e67d4a91d68b9c1e1edf131548ede453b90a3
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-04-03 12:21:48 +0530

    Updated Room Page

--- controller/pg.js
+++ controller/pg.js
@@ -196,8 +196,8 @@ module.exports.addBooking = async (req, res, next) => {
         console.log(roomAvailable)
         if (!roomAvailable) {
             console.log('Room is not available for the specified date and time range');
+            res.redirect('/platformAnchorage/roomScheduling');
         }
-
         else {
             await rooms.updateOne(
                 { roomNumber: roomNumber },
@@ -213,11 +213,12 @@ module.exports.addBooking = async (req, res, next) => {
                 }
                 // options
             );
+            res.redirect('/platformAnchorage/roomScheduling');
         }
+        
     }
-    res.redirect('/platformAnchorage/roomScheduling');
-    // let sample = await rooms.find({})
-    // console.log(sample[2].bookings);
+    
+    
 }
 
 
