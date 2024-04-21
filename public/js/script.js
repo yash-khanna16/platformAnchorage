bf981418d2947a37086e6b21e81cffd9e452c285
Yash Khanna <yashkhanna16062002@gmail.com>
2024-04-22 01:51:17 +0530

    added edit booking button only

--- public/js/script.js
+++ public/js/script.js
@@ -19,36 +19,6 @@ $(() => {
   });
 });
 
-// show or hide details
-// function toggleDetails() {
-//   alert("i nkjdsbvvbks");
-//   var details = document.querySelector(".product-details");
-//   var button = document.querySelector("button");
-
-//   if (details.style.display === "none") {
-//     details.style.display = "block";
-//     button.textContent = "Hide Details";
-//   } else {
-//     details.style.display = "none";
-//     button.textContent = "Show More Details";
-//   }
-// }
-
-// $(document).ready(function () {
-//   $("#toggleButton").click(function () {
-//     $("#product-details").toggle();
-//   });
-// });
-
-// document.getElementById("toggleButton").addEventListener("onclick", function () {
-//   var details = document.getElementById("details");
-//   console.log(hiii);
-//   if (details.style.display === "none") {
-//     details.style.display = "block";
-//   } else {
-//     details.style.display = "none";
-//   }
-// });
 
 $(() => {
   const toggleButton = $(".toggleButton");
