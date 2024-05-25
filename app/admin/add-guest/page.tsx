b3453a73f05916f339b21a61d9612dfea343c7ee
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-05-26 00:06:06 +0530

    add guest and send emails done

--- app/admin/add-guest/page.tsx
+++ app/admin/add-guest/page.tsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 function AddGuest() {
   return (
-    <div>GuestDetails</div>
+    <div className='w-[1000px] mx-auto pt-10 px-10'>
+      <h1 className=' text-3xl font-bold'>Enter details of guests </h1>
+      <form className=' mx-auto'>
+        <div className='w-full  grid grid-cols-2 gap-7 mx-auto mt-[40px] '>
+          <div className='flex flex-col'>
+            <label htmlFor='guestName'>Name</label>
+            <input type="text" name='guestName' placeholder='Enter Name' id="guestName" className='border-2 mt-2 rounded-lg p-2' />
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor='guestPhone'>Phone No.</label>
+            <input type="tel" name='guestPhone' placeholder='Enter Phone No.' id="guestPhone" className='border-2 mt-2 rounded-lg p-2' />
+          </div>
+          <div className='flex  flex-col'>
+            <label htmlFor='guestEmail'>Email Address</label>
+            <input type="email" name='guestEmail' placeholder='Enter Email Address' id="guestEmail" className='border-2 mt-2 rounded-lg p-2' />
+          </div>
+          <div className='flex  flex-col'>
+            <label htmlFor='guestCompany'>Company</label>
+            <input type="text" name='guestCompany' placeholder='Enter Company' id="guestCompany" className='border-2 mt-2 rounded-lg p-2' />
+          </div>
+          <div className='flex  flex-col'>
+            <label htmlFor='guestRank'>Rank</label>
+            <input type="text" name='guestRank' placeholder='Enter Rank' id="guestRank" className='border-2 mt-2 rounded-lg p-2' />
+          </div>
+        </div>
+        <button type='submit' className='mt-8 font-semibold px-3 py-2 w-[300px]  bg-[#1A80E5] rounded-md text-white p-2'>Add Guest</button>
+      </form>
+    </div>
   )
 }
 
--- app/admin/send-emails/page.tsx
+++ app/admin/send-emails/page.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function SendEmails() {
   return (
-    <div>SendEmails</div>
+    <div className='w-[700px] mx-auto pt-10 px-10'>
+      <h1 className=' text-3xl font-bold'>Send Emails </h1>
+      <form className=' mx-auto'>
+        <div className=' mt-[40px] '>
+          <div className='flex flex-col'>
+            <label htmlFor='sendEmail'>Email</label>
+            <input type="text" name='sendEmail' placeholder='Subject' id="sendEmail" className='border-2 mt-2 rounded-lg p-3' />
+          </div>
+          <div className='flex mt-5 flex-col'>
+            <textarea name='guestCompany' rows={8} placeholder='Write a message here....' id="guestCompany" className='border-2 mt-2 rounded-lg p-4 bg-gray-100' />
+          </div>
+        </div>
+        <button type='submit' className='mt-8 block font-semibold w-full mx-auto bg-[#1A80E5] rounded-md text-white px-3 py-[7px]'>Send Email</button>
+      </form>
+    </div>
   )
 }
 
