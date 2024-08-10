c813cb6ecab9f46146508cd2be4f3b81150c6f9a
Deepanshu Pal <dp5114@dseu.ac.in>
2024-08-10 13:34:39 +0530

    web socket fix

--- app/actions/websocket.ts
+++ app/actions/websocket.ts
@@ -5,7 +5,8 @@ let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io(process.env.BACKEND_URL || "http://localhost:8000");
+    socket = io(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000");
+    socket.emit("join_room", process.env.NEXT_PUBLIC_ROOM_CODE || "test")
   }
   return socket;
 };
