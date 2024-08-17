8805a33e6f1d4516540593734830b4d477b286a9
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-08-18 02:30:39 +0530

    socket changes

--- app/actions/websocket.ts
+++ app/actions/websocket.ts
@@ -1,10 +1,11 @@
-// socket.ts
+
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
+    console.log(process.env.NEXT_PUBLIC_ROOM_CODE);
     socket = io(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000");
     socket.emit("join_room", process.env.NEXT_PUBLIC_ROOM_CODE || "test")
   }
