3ee31ca86ce0a03213ab7eaf3a890c1a5ea64a81
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-08-18 03:21:30 +0530

    clg changed

--- app/actions/websocket.ts
+++ app/actions/websocket.ts
@@ -5,8 +5,7 @@ let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    console.log(process.env.NEXT_PUBLIC_ROOM_CODE);
-    console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
+    console.log(process.env);
     socket = io(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000");
     socket.emit("join_room", process.env.NEXT_PUBLIC_ROOM_CODE || "test")
   }
