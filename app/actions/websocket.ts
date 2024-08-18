e0d2767d744282a177b80e4d4fb7333e19784256
Anant <99551681+anantbansa1@users.noreply.github.com>
2024-08-18 17:43:04 +0530

    Update websocket.ts please revert back

--- app/actions/websocket.ts
+++ app/actions/websocket.ts
@@ -17,9 +17,9 @@ const initializeSocket = async (): Promise<void> => {
     try {
       const secret = await loadConfig();
       console.log(secret);
-      //fix this later temp change
+      //fix this later temp change please change this
       socket = io(secret.BACKEND_URL || "https://platformanchoragebackend-4av0.onrender.com");
-      socket.emit("join_room", secret.ROOM_CODE || "test");
+      socket.emit("join_room", secret.ROOM_CODE || "4398vnksd843jf7");
     } catch (error) {
       console.error("Error initializing socket:", error);
     }
