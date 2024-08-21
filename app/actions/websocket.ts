a0a11e39ab292097e93545edc3b5d2a0977a853d
Deepanshu Pal <dp5114@dseu.ac.in>
2024-08-21 16:09:53 +0530

    testing envs

--- app/actions/websocket.ts
+++ app/actions/websocket.ts
@@ -17,6 +17,8 @@ const initializeSocket = async (): Promise<void> => {
     try {
       const secret = await loadConfig();
       console.log(secret);
+      console.log("env backend: ", process.env.NEXT_PUBLIC_BACKEND_URL)
+      console.log("room code: ", process.env.NEXT_PUBLIC_ROOM_CODE)
       //fix this later temp change please change this
       socket = io(secret.BACKEND_URL || "https://platformanchoragebackend-4av0.onrender.com");
       socket.emit("join_room", secret.ROOM_CODE || "4398vnksd843jf7");
